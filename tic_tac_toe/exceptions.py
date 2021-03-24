66193972faf6419c8abdd71013dac86e2f843eb1
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-24 16:38:34 +0200

    Moved all exceptions into exceptions.py

---
+++ tic_tac_toe/exceptions.py
@@ -0,0 +1,14 @@
+class GameAreaException(Exception):
+    pass
+
+
+class GameAreaUnitException(GameAreaException):
+    pass
+
+
+class GameAreaIndexException(GameAreaException):
+    pass
+
+
+class GameException(Exception):
+    pass
